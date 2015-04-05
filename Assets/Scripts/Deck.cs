11b90b13e7dd78983e9b73177e3d33f284c614f0
JaxFirehart <Kylecondo@gmail.com>
2015-04-05 11:47:34 -0600

    Finished the Card prefab Created materials for all of the cards, I did not include suits because suits don't matter in Idiot and it highly simplified card generation. Special cards, (2, 5, 10) have no numerical value and instead have a symbol showing what they do. We might consider adding them as truly special cards instead of having them overwrite 2/5/10. The Jack face card has a picture of Jack Skellington on it and J's in the corners, I would like to do something similar for Queen, King and Ace but couldn't come up with ideas I liked. Created object script "Card" and static script "Deck" but they don't do anything yet

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Assets.Scripts
+{
+    public static class Deck
+    {
+        static List<Card> Cards;
+
+        public static void Initialize()
+        {
+            Cards = new List<Card>();
+        }
+    }
+}
