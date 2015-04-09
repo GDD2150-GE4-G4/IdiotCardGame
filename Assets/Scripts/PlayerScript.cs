070c39f7fb01fbdb75979e6f304ccc0ad6056cec
JaxFirehart <Kylecondo@gmail.com>
2015-04-09 12:16:37 -0600

    Created Game script to handle the game overview Created Player script and prefab to better organized a player's resources and allow the game to monitor who is playing

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class PlayerScript : MonoBehaviour
+    {
+        public Hand Hand;
+        public BlindDeckScript[] Blinds;
+    }
+}
