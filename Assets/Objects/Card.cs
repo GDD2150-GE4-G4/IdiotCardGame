07e2be155d5f89220612e04fc585a9835e520771
Aaron <abatilo@uccs.edu>
2015-04-14 19:43:58 -0600

    Started re-doing some of the objects

---
+++ Assets/Objects/Card.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System;
+
+public class Card : IComparable<Card>, IEquatable<Card> {
+
+	private Material myMaterial = null;
+	private ushort myValue = 0;
+	private bool myIsFaceUp = true;
+	private SpecialCardAction myAction = SpecialCardAction.None;
+
+	public Material GetFaceMaterial()
+	{
+		return myMaterial;
+	}
+	
+	public void SetFaceMaterial(Material inMaterial)
+	{
+		myMaterial = inMaterial;
+	}
+	
+	public ushort GetValue()
+	{
+		return myValue;
+	}
+	
+	public void SetValue(ushort inValue)
+	{
+		myValue = inValue;
+	}
+	
+	public bool IsFaceUp()
+	{
+		return myIsFaceUp;
+	}
+	
+	public void SetIsFaceUp(bool inIsFaceUp)
+	{
+		myIsFaceUp = inIsFaceUp;
+	}
+
+	public SpecialCardAction GetAction ()
+	{
+		return myAction;
+	}
+
+	public void SetAction (SpecialCardAction inAction)
+	{
+		myAction = inAction;
+	}
+
+	public int CompareTo(Card inOther)
+	{
+		return this.GetValue () - inOther.GetValue ();
+	}
+	
+	public bool Equals(Card inOther)
+	{
+		return CompareTo (inOther) == 0;
+	}
+	
+	public override bool Equals(object inOther)
+	{
+		return Equals (inOther as Card);
+	}
+	
+	public override int GetHashCode ()
+	{
+		return base.GetHashCode ();
+	}
+	
+	public override string ToString ()
+	{
+		return GetValue().ToString();
+	}
+}
---
+++ Assets/Objects/Deck.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Deck : ICardCollection {
+
+	private static Deck myInstance = null;
+
+	public Deck()
+	{
+		if (myInstance == null)
+			myInstance = new Deck ();
+	}
+}
